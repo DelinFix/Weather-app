5d3b910a165b765427d91d5b0aa4ffbdd30474e4
DelinFix <denis.basenko@yandex.ru>
2022-05-25 16:34:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState} from "react"
 
 function App() {
+    const [query, setQuery] = useState('')
+    const [weather, setWeather] = useState('')
+    const [title, setTitle] = useState('Enter the name of the city and press Enter')
+
+    const search = event => {
+        if(event.key === "Enter") {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=9ff978828c0dcd4087017012b6eb3e5c`)
+                .then(res => res.json())
+                .then(result => {
+                    if(result.message === "city not found") {
+                        setTitle('Error, city not found, try again')
+                        setWeather(result)
+                    } else {
+                        setWeather(result)
+                        setQuery('')
+                    }
+                })
+        }
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className={(typeof weather.main != "undefined")
+          ? ((weather.main.temp > 17)
+              ? 'background warm'
+              : 'background')
+          : 'background'}>
+          <div className={
+              (typeof weather.main != "undefined")
+                  ? ((weather.main.temp > 17)
+                  ? 'app warm'
+                  : 'app')
+                  : 'app'}>
+              <main>
+                  <div className="search-box">
+                      <input
+                          type="text"
+                          className="search-box__bar"
+                          placeholder="Search..."
+                          onChange={e => setQuery(e.target.value)}
+                          value={query}
+                          onKeyPress={search}
+                      />
+                  </div>
+                  {(typeof  weather.main != "undefined") ?
+                      (
+                          <div>
+                              <div>
+                                  <div className="location-box">
+                                      <div className="location-box__name">
+                                          {weather.name}, {weather.sys.country}
+                                      </div>
+                                      <div className="location-box__date">
+                                          {String(new window.Date()).slice(3,15)}
+                                      </div>
+                                  </div>
+                              </div>
+                              <div className="weather-box">
+                                  <div className="weather-box__temp">
+                                      {Math.round(weather.main.temp)}° C
+                                  </div>
+                                  <div className="weather-box__weather">
+                                      {weather.weather[0].main}
+                                  </div>
+                              </div>
+                          </div>)
+                      : (<h1 className="title">{title}</h1>)
+                  }
+              </main>
+          </div>
+      </div>
+  )
 }
 
-export default App;
+export default App
